395e73c26d490a0f436d6d1053f98164350ecac7
aBock4 <arbockhorst@gmail.com>
2023-02-13 20:44:28 -0500

    Lab 6

--- node/server.js
+++ node/server.js
@@ -11,6 +11,24 @@ http.createServer((req, res) => {
     });
   } else if(req.url.match("/sysinfo")) {
     myHostName=os.hostname();
+	//get total uptime and convert into days, hours, minutes, and seconds
+	uptime = os.uptime();
+	var uptimeDays = Math.floor(uptime/86400);
+	var uptimeHours = Math.floor((uptime - uptimeDays * 86400) / 3600);
+	var uptimeMinutes = Math.floor((uptime - uptimeDays * 86400 - uptimeHours * 3600) / 60);
+	var uptimeSeconds = Math.floor(uptime - uptimeDays * 86400 - uptimeHours * 3600 - uptimeMinutes * 60);
+	
+	//get total memory and convert to MB
+	var totalByte = os.totalmem();
+	var totalMegaBit = totalByte * 0.000001
+	
+	//get free memory and convert into MB
+	var freeByte = os.freemem();
+	var freeMegaBit = freeByte * 0.000001
+	
+	//get number of CPU's
+	var cpus = os.cpus().length;
+	
     html=`
     <!DOCTYPE html>
     <html>
@@ -20,10 +38,10 @@ http.createServer((req, res) => {
       <body>
         <p>Hostname: ${myHostName}</p>
         <p>IP: ${ip.address()}</p>
-        <p>Server Uptime: </p>
-        <p>Total Memory: </p>
-        <p>Free Memory: </p>
-        <p>Number of CPUs: </p>
+        <p>Server Uptime: Days: ${uptimeDays}, Hours: ${uptimeHours}, Minutes: ${uptimeMinutes}, Seconds: ${uptimeSeconds}</p>
+        <p>Total Memory: ${totalMegaBit} MB</p>
+        <p>Free Memory: ${freeMegaBit} MB</p>
+        <p>Number of CPUs: ${cpus}</p>
       </body>
     </html>`
     res.writeHead(200, {"Content-Type": "text/html"});
