4098505d83d0166c2f1d7582e38cca9fcd2ecdd1
aBock4 <arbockhorst@gmail.com>
2023-02-20 09:05:09 -0500

    Added Project 1

---
+++ project1/Project 1.ps1
@@ -0,0 +1,11 @@
+﻿#This is a powershell script
+#This script tells the user the current user and the time
+
+#set the current user to a variable
+$user = $env:USERNAME
+
+#set the current time (hour and minutes only) to a variable
+$time = Get-Date -Format "hh:mm"
+
+#print friendly message to console/host with current user and current time
+Write-Host("The current user is: " + $user + ". The current time is: " + $time)
