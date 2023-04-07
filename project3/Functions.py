d3ff7a308859e3418d271324e3d8dcad560fa862
aBock4 <82166772+aBock4@users.noreply.github.com>
2023-04-07 15:14:19 -0400

    Update Functions.py

--- project3/Functions.py
+++ project3/Functions.py
@@ -1,9 +1,11 @@
 import requests
 from tabulate import tabulate
 
+#Documentation for API found at: https://www.openbrewerydb.org/documentation
 
+#function to show choices in a table for user
 def showChoices():
-    #array of search choices
+    #json with search choices
     choices = [{'choice': "name", 'meaning': 'Search by name of brewery'},{'choice': "city", 'meaning': 'Search by city of brewery'},{'choice': "state", 'meaning': 'Search by state of brewery'},{'choice': "type", 'meaning': 'Search by type of brewery'}]
     
     #create a table with 3 headers
@@ -22,6 +24,7 @@ def showChoices():
     #Print the table to the console
     print(tabulate(table, tablefmt='fancy_grid'))
 
+#function to handle bad responses
 def responseStatus(response):
     #some error handling
     if response.status_code > 300:
@@ -30,13 +33,14 @@ def responseStatus(response):
         json = response.json()
         return json
 
+#function to print the tables
 def printTable(json):
-    #create a table with 3 headers
+    #create a table with 6 headers
     table = [
         ['Brewery:', 'Street:', 'City:', 'State:', 'Zip Code:', 'Website URL:']
     ]
 
-    #add brewery, street, and zip code to each row
+    #add brewery, street, city, state, zip code, and website url to each row
     for model in json:
         row = [
             model['name'],
@@ -50,42 +54,62 @@ def printTable(json):
 
     #Print the table to the console
     print(tabulate(table, tablefmt='fancy_grid'))
+
+#function to search the api by names
 def searchByName(name):
-    response = requests.get('https://api.openbrewerydb.org/breweries?by_name=' + name.lower() + '&per_page=100')
+    #set up url and get json response
+    #limit the results to 200
+    response = requests.get('https://api.openbrewerydb.org/breweries?by_name=' + name.lower() + '&per_page=200')
     
+    #see if the response is good
     json = responseStatus(response)
     
+    #call the print table function 
     printTable(json)
     
+#function to search the api by city
 def searchByCity(city):
+    #set up url and get json response
     response = requests.get('https://api.openbrewerydb.org/breweries?by_city=' + city.lower())
     
+    #see if the response is good
     json = responseStatus(response)
     
+    #call the print table function 
     printTable(json)
 
+#funcion to search the api by state
 def searchByState(state):
-    #cap this at 100 because it can be lengthy
-    response = requests.get('https://api.openbrewerydb.org/breweries?by_state=' + state.lower() + '&per_page=100')
+    #set up url and get json response
+    #cap this at 200 because it can be lengthy
+    response = requests.get('https://api.openbrewerydb.org/breweries?by_state=' + state.lower() + '&per_page=200')
     
+    #see if the response is good
     json = responseStatus(response)
     
+    #call the print table function 
     printTable(json)
 
+#function to search the api by type
 def searchByType(bType):
-    #cap this at 100 because it can be lengthy
-    response = requests.get('https://api.openbrewerydb.org/breweries?by_type=' + bType.lower() + '&per_page=100')
+#set up url and get json response
+    #cap this at 200 because it can be lengthy
+    response = requests.get('https://api.openbrewerydb.org/breweries?by_type=' + bType.lower() + '&per_page=200')
     
+    #see if the response is good
     json = responseStatus(response)
     
+    #call the print table function 
     printTable(json)
 
+#function to show the choices for the types of breweries
 def showTypes():
     #create a table with 3 headers
     table = [
         ['Choice:', 'What it means:']
     ]
 
+    #json with choices and meanings of types/options
     types = [{'choice': 'micro', 'meaning': 'Most craft breweries. For example, Samual Adams is still considered a mircro brewery.'},{'choice': 'nano', 'meaning': 'An extremely small brewery which typically only distributes locally.'},{'choice': 'regional', 'meaning': 'A regional location of an expanded brewery. Ex. Sierra Nevada’s Asheville, NC location.'},{'choice': 'brewpub', 'meaning': 'A beer-focused restaurant or restaurant/bar with a brewery on-premise.'},{'choice': 'large', 'meaning': 'A very large brewery. Likely not for visitors. Ex. Miller-Coors'},{'choice': 'planning', 'meaning': 'A brewery in planning or not yet opened to the public.'},{'choice': 'bar', 'meaning': 'A bar. No brewery equipment on premise.'},{'choice': 'contract', 'meaning': 'A brewery that uses another brewery’s equipment.'},{'choice': 'propietor', 'meaning': 'Similar to contract brewing but refers more to a brewery incubator'},{'choice': 'closed', 'meaning': 'A location which has been closed.'}]
     
     #add data in
